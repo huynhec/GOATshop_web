cabd4621eba06be3cc16e9b4c62b97f69674ac0d
huynhec <huynhbarca@gmail.com>
2024-01-30 10:15:44 +0700

    file index nè em

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOATshop</title>
+
+    <link rel="stylesheet" href="./assets/bootstrap-5.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/mycss.css">
+    <!-- boostrap -->
+
+
+
+</head>
+
+<body>
+<?php require "./crud/index.php";?>
+<script src="./assets/jquery-3.7.1.min.js"></script>
+<script src="./assets/bootstrap-5.3.2/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
