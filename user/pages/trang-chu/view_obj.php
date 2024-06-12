6720744ed645681f94b76bbbf3a530d0ed1c68dd
huynhec <huynhbarca@gmail.com>
2024-06-12 10:29:06 +0700

    fix xem nhanh san pham

--- user/pages/trang-chu/view_obj.php
+++ user/pages/trang-chu/view_obj.php
@@ -4,7 +4,6 @@ require_once '../../../model/AnhSpModel.php';
 require_once '../../../model/CommonModel.php';
 require_once '../../../model/DonGiaModel.php';
 
-$dg = new DonGiaModel();
 $sp = new SanPhamModel();
 $anhSp = new AnhSpModel();
 $cm = new CommonModel();
@@ -21,11 +20,20 @@ $productArrays = [
     '4' => $sp__Get_Top9_Attribute_4
 ];
 ?>
+<style>
+    .current-price-view {
+        color: #d9534f !important;
+        font-weight: bold !important ;
+        font-size: larger;
+    }
+</style>
 <div class="product-grid" id="product-grid">
 
     <?php
     function renderProducts($products, $anhSp)
     {
+        $dg = new DonGiaModel();
+
         foreach ($products as $item) {
             $anhSp__Get_By_Id_Sp_First = $anhSp->AnhSp__Get_By_Id_Sp_First($item->masp);
     ?>
@@ -35,14 +43,16 @@ $productArrays = [
                 </a>
                 <div class="product-info">
                     <h2><?= $item->tensp ?></h2>
-                    <p>Sắp mở bán</p>
+                    
+                    <span class="current-price-view"><?= number_format($dg->ShowDonGia__Get_By_Id_Spdg($item->masp)) ?>₫</span>
+
                     <div class="product-actions text-center clearfix">
                         <div>
-                            <button type="button" class="btnQuickView quick-view medium--hide small--hide" data-handle="/products/nike-mercurial-vapor-13-academy-tf-2">
+                            <button type="button" class="btnQuickView quick-view medium--hide small--hide" data-handle="/products/nike-mercurial-vapor-13-academy-tf-2" onclick="return view('<?= $item->masp ?>','<?= $item->maloai ?>','<?= $_SESSION['user']->makh ?? 0; ?>')">
                                 <span><i class="fa fa-search-plus" aria-hidden="true"></i></span>
                             </button>
-                            <button type="button" class="btnBuyNow buy-now medium--hide small--hide" data-id="1085955545"><span>Mua ngay</span></button>
-                            <button type="button" class="btnAddToCart add-to-cart medium--hide small--hide" data-id="1085955545">
+                            <button type="button" class="btnBuyNow buy-now medium--hide small--hide" data-id="1085955545" onclick="buyNow('<?= $item->masp ?>')"><span>Mua ngay</span></button>
+                            <button type="button" class="btnAddToCart add-to-cart medium--hide small--hide" data-id="1085955545" onclick="addCartSize('<?= $item->masp ?>')">
                                 <span><i class="fa fa-cart-plus" aria-hidden="true"></i></span>
                             </button>
                         </div>
